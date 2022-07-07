2d3adde93b9f8fabc97a5b7f10f8e7a46543a3f8
Tiago <tiagohucs@gmail.com>
2022-07-07 11:49:50 -0300

    inicial

---
+++ hr-worker/src/main/java/com/example/hrworker/entities/Worker.java
@@ -0,0 +1,27 @@
+package com.example.hrworker.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "TB_WORKER")
+public class Worker implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+}
---
+++ hr-worker/src/main/java/com/example/hrworker/resources/WorkerResource.java
@@ -0,0 +1,25 @@
+package com.example.hrworker.resources;
+
+import com.example.hrworker.entities.Worker;
+import com.example.hrworker.repositories.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResource {
+
+    @Autowired
+    private WorkerRepository workerRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Worker>> findALl(){
+        List<Worker> list = workerRepository.findAll();
+        return ResponseEntity.ok(list);
+    }
+}
