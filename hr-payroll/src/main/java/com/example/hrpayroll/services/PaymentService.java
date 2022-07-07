83bb0ac80e70f14a870acac0dc0c71b8b7293c52
Tiago <tiagohucs@gmail.com>
2022-07-07 15:08:17 -0300

    payment complete

--- hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
@@ -1,12 +1,33 @@
 package com.example.hrpayroll.services;
 
 import com.example.hrpayroll.entities.Payment;
+import com.example.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(Long workerId, int days){
-        return Payment.builder().name("Mock").dailyIncome(250.0).days(days).build();
+        Map<String,String> uriVariables = new HashMap<>();
+        uriVariables.put("id", ""+workerId);
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}",Worker.class,uriVariables);
+
+        return Payment.builder()
+                .name(worker.getName())
+                .dailyIncome(worker.getDailyIncome())
+                .days(days)
+                .build();
     }
 }
