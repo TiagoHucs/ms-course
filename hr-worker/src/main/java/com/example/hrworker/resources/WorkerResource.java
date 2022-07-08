f522274b1b0a568dd6abf9773833f0b555cfff77
Tiago <tiagohucs@gmail.com>
2022-07-08 12:16:44 -0300

    hr-worker as config server client

--- hr-worker/src/main/java/com/example/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/example/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import com.example.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,9 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment env;
 
@@ -25,14 +29,21 @@ public class WorkerResource {
 
     @GetMapping
     public ResponseEntity<List<Worker>> findALl(){
+        logger.info("findALl in worker port = " + env.getProperty("local.server.port"));
         List<Worker> list = workerRepository.findAll();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs(){
+        logger.info("Getting configs at ${test.config} = " + testConfig);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
 
-        logger.info("PORT = " + env.getProperty("local.server.port"));
+        logger.info("findById in worker port = " + env.getProperty("local.server.port"));
 
         Worker wr = workerRepository.findById(id).get();
         return ResponseEntity.ok(wr);
