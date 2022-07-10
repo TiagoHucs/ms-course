cd23015dc8933cbe8035f7c1ee33db46f224d260
Tiago <tiagohucs@gmail.com>
2022-07-10 18:17:08 -0300

    jwt part2 , login ok

--- hr-oauth/src/main/java/com/example/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/example/hroauth/entities/User.java
@@ -1,16 +1,21 @@
 package com.example.hroauth.entities;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
-public class User {
+public class User implements UserDetails {
 
     private Long id;
     private String name;
@@ -18,4 +23,34 @@ public class User {
     private String password;
     private Set<Role> roles = new HashSet<>();
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
